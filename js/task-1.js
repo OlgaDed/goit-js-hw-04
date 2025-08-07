fa6b5a18d744cc0e5065226ee51a7b15023bcda1
OlgaDed <garmoneya@ukr.net>
2025-08-08 00:49:19 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function isEnoughCapacity(products, containerSize) {
+  const totalQuantity = Object.values(products).reduce(
+    (sum, amount) => sum + amount,
+    0
+  );
+  return totalQuantity <= containerSize;
+}
+
+isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8); // повертає true
+isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12); // повертає false
+isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14); // повертає true
+isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7); // повертає false

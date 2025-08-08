69bebb778f871e87dc84c9bf30f2bcfe84d253a1
OlgaDed <garmoneya@ukr.net>
2025-08-08 15:20:13 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ function isEnoughCapacity(products, containerSize) {
   return totalQuantity <= containerSize;
 }
 
-isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8); // повертає true
-isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12); // повертає false
-isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14); // повертає true
-isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7); // повертає false
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // повертає true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // повертає false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // повертає true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // повертає false
